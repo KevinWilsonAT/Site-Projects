4ca90341490a372f12000e6a8b6c074827732c6e
Kevin Wilson <104521683+KevinWilsonAT@users.noreply.github.com>
2024-01-29 20:15:20 -0300

    updates of folders 29-01-24

--- landing-pages/site-starbucks/src/scripts/script.js
+++ landing-pages/site-starbucks/src/scripts/script.js
@@ -5,16 +5,16 @@ let circulo = document.querySelector(".circulo")
         let rosa = document.querySelector(".rosa")
 
         verde.addEventListener("click", () => {
-            imgCopo.src = "images/img1.png"
+            imgCopo.src = "src/img/img1.png"
             circulo.style.background = "#017143"
         })
 
         amarelo.addEventListener("click", () => {
-            imgCopo.src = "images/img2.png"
+            imgCopo.src = "src/img/img2.png"
             circulo.style.background = "#eb7495"
         })
 
         rosa.addEventListener("click", () => {
-            imgCopo.src = "images/img3.png"
+            imgCopo.src = "src/img/img3.png"
             circulo.style.background = "#d752b1"
         })
